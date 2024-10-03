c740dacf035b9cc55738b00c9b4bacefc0bb1bd0
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-10-03 22:49:00 +0545

    uploadthings url img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 import Link from "next/link";
 
+const mockUrls =
+  [
+    
+    "https://utfs.io/f/Z070XchsY2SjbaYvxtLGOg9NJp1MrFPVQfaniwvBlmdRSjCZ",
+    "https://utfs.io/f/Z070XchsY2Sj4a7mSVwu0g2vB5rFHRh3KpUeAId9Yn1ZfOqo",
+    "https://utfs.io/f/Z070XchsY2SjtfqzTwnQnbmIU9gA36yDNKRWHh2iavLfYlos"
+  ]
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}))
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-Hello gallery in progress
+    <main className="bg-black text-white">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages].map((image) => (
+          <div className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
+
+
+      Hello gallery in progress
     </main>
   );
 }
